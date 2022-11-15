901359b97e38ce7b3c000c688b2a486c337bf0d4
Muhammad Rifaldi Judri <muhamadrifaldi145@gmail.com>
2022-11-15 23:52:56 +0700

    Update Login Magic Link

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom"
-
-import App from './App'
-import Dashboard from './Dashboard'
-import InputData from './InputData'
-import TambahDataMin from './TambahDataMin'
-import TambahDataMax from './TambahDataMax'
-import Grafik from './Grafik'
-
-import { ChakraProvider } from '@chakra-ui/react'
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/InputData" element={<InputData />} />
-        <Route path="/TambahDataMin" element={<TambahDataMin />} />
-        <Route path="/TambahDataMax" element={<TambahDataMax />} />
-        <Route path="/Grafik" element={<Grafik />} />
-      </Routes>
-    </BrowserRouter>
-  </ChakraProvider>
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";  
+ 
+ReactDOM.render(   
+    <App /> ,
+  document.getElementById("root")
 );
